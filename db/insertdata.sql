894a28d1db87510db2002b8f15f56bdba0cf0121
jaja <jbjansen97@gmail.com>
2019-04-21 15:11:53 -0400

    Added bike SQL data

--- db/insertdata.sql
+++ db/insertdata.sql
@@ -7,6 +7,53 @@ INSERT INTO regions (name, descript, diags, url) VALUES ('', '', '', '')
 
 */
 
+/* 
+https://www.consumerreports.org/cro/bikes/buying-guide/index.htm
+NOTE: All price values are in USD
+*/
+
+INSERT INTO bikes (name, descript, avgprice, url) VALUES (
+  'road', 
+  'The road bike is a slim form-factor bicycle, meant for smooth terrains such as asphalt, bricks, concrete, etc. Often used for races and commutes, they are often used for fast transport.', 
+  3750, 
+  'https://www.wigglestatic.com/product-media/100375136/Brand-X-Road-Bike-Road-Bikes-Black-2017-BRNDXROADXL-0.jpg?w=960&h=430&a=7')
+
+INSERT INTO bikes (name, descript, avgprice, url) VALUES (
+  'mountain',
+  'The mountain bike is an all terrain vehicle. The tire tread and pressure rugged and adjustable, respecitvely, for hanlding various terrains. Mountain bikes are highly popular in downhill races and trekking through forests.', 
+  1000, 
+  'https://tredz.azureedge.net/prodimg/109251_1_Zoom.jpg')
+
+INSERT INTO bikes (name, descript, avgprice, url) VALUES (
+  'bmx', 
+  'The BMX bike is known for its small form factor and ability to handle stunts. This bike is highly popularized for racing and stunt courses and parks.', 
+  400, 
+  'https://cdn.shopify.com/s/files/1/0062/5708/2479/products/239f35ac-80e2-4537-894f-bdac15bfcd40_large.jpg?v=1554991657')
+
+INSERT INTO bikes (name, descript, avgprice, url) VALUES (
+  'hybid', 
+  'As a compromise between the mountain and road bikes, the Hybrid bike takes aspects of each for an optimized ride. Typical models use the handlebar and frame structures of the mountain bike, while maintaining the gearing and tires of a road bike. Some models will have additional treading for handling less-smotth terrain and even help shed rain.', 
+  1000, 
+  'https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/2/2018/02/Screen-Shot-2018-10-10-at-12.21.15.png')
+
+INSERT INTO bikes (name, descript, avgprice, url) VALUES (
+  'electric', 
+  'Electric bikes, which are highly popularized in the US and Germany, are the latest eco-friendly addition to the realm of bicycles. Sporting a battery and motor, these bikes are build to assist or even drive the rider to their destination. Most are rechargable, many featuring Lithium Ion or Polymer batteries', 
+  1500, 
+  'https://electricbikereview.com/wp-content/assets/2017/05/voltbike-enduro-electric-bike-review.jpg')
+
+INSERT INTO bikes (name, descript, avgprice, url) VALUES (
+  'tandem', 
+  'For those who enjoy riding with others, tandem bikes are built for team work. Featuring two or more seats, tandem bikes are geared for eery rider in-line.', 
+  400, 
+  'https://static.evanscycles.com/production/bikes/tandems/product-image/484-319/ridgeback-velocity-2018-tandem-bike-grey-EV320438-7000-1.jpg')
+
+INSERT INTO bikes (name, descript, avgprice, url) VALUES (
+  'cruiser', 
+  'The classical crusier has many distinct features, including the high-rise handlebars, arched frame and wide road tires. These bikes are best for flat terrains when you only need to get from one place to another at a crusing speed.', 
+  450, 
+  'https://www.bikensurfdubai.com/wp-content/uploads/2015/07/BREWSTER_CRUSIER_2015_SEAMLESS_SIDE_WEB_1024x1024.jpg')
+
 /* Linker Tables (bikes -> regions, regions -> tools, regions -> parts)
 
 INSERT INTO <biketype>_regions (id) VALUES ((SELECT id from regions WHERE name='<region_name>'))
